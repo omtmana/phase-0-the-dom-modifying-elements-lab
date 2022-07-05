cd65886f7cbd5575b7004460b6e231bc74f3ff22
Olivia <oliviatmanalastas@gmail.com>
2022-07-05 11:46:46 -0400

    commit tree 608eeeb91960f2d3a973940779eb90e2f6b20c47

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Write your code here!
-document.getElementById('main').remove();
+// document.getElementById('main').remove();
 
-var newHeader = document.createElement("h1");
-newHeader.id = 'victory'
-newHeader.innerHTML = "YOUR-NAME is the champion"
+// var newHeader = document.createElement("h1");
+// newHeader.id = 'victory'
+// newHeader.innerHTML = "YOUR-NAME is the champion"
+
+document.getElementById('main').remove();
+const newHeader = document.getElementById('victory')
+newHeader.innerHTML = 'Olivia is the champion'
